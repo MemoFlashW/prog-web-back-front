ec7dc3256630ed555e0b843c816eb757a20df008
Guillermo <146399926+MemoFlashW@users.noreply.github.com>
2025-11-25 13:03:13 -0700

    Add files via upload

---
+++ backend/build/schema/personal.Schema.js
@@ -0,0 +1,10 @@
+import { z } from 'zod';
+const telefonoRegex = new RegExp(/^[+]?(\d{1,2})?[\s.-]?\(?\d{3}\)?[\s.-]?\d{3}[\s.-]?\d{4}$/);
+//validaciones con Zod - construir un schema
+export const personalSchema = z.object({
+    nombre: z.string().min(2).max(200),
+    direccion: z.string(),
+    telefono: z.string().regex(telefonoRegex),
+    //telefono:z.string().min(10).max(15),
+    estatus: z.number().int().positive('Solo numeros positivos').min(1).max(2)
+});
---
+++ backend/src/index.ts
@@ -0,0 +1,28 @@
+import express from 'express';
+import cors from 'cors';
+//Creamos la aplicación a través del paquete express
+//y llamamos a su constructor
+const app = express();
+//Configurar rutas para el acceso a personal
+import personalRutas from './routes/personalRutas.js';
+//Todo lo que regresa al usuario es tipo JSON
+app.use(express.json());
+app.use(cors()); //Permite al frontend conectarse
+//Puerto para escuchar la petición del frontend
+const PUERTO = 3001;
+
+//Activar la ruta base
+app.use('/api/personal',personalRutas);
+
+//Ruta
+// app.get('/',(_req,res) =>{
+//     res.send("Ruta raíz del servidor API");
+// })
+// app.get('/hola',(_req,res) =>{
+//     res.send("Ruta hola del servidor API");
+// })
+
+//Encendemos el servidor y lo ponemos en escucha
+app.listen(PUERTO,() =>{
+    console.log(`Servidor encendido y escuchando en el puerto ${PUERTO}`)
+})
