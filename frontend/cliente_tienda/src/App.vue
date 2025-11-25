a6874f009c78c4ae1db5121808a3fc576cfdf50a
Guillermo <146399926+MemoFlashW@users.noreply.github.com>
2025-11-25 13:03:59 -0700

    Add files via upload

---
+++ frontend/cliente_tienda/src/App.vue
@@ -0,0 +1,12 @@
+<script setup lang="ts">
+import { RouterView } from 'vue-router';
+import MenuVue from './modulos/principal/vistas/MenuVue.vue';
+
+</script>
+
+<template>
+    <header>
+        <MenuVue/>
+    </header>
+    <RouterView/>
+</template>
