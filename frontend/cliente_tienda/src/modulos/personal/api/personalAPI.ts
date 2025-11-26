a33c158203bafca1de53d49f07deaaab6ece1a28
Guillermo <146399926+MemoFlashW@users.noreply.github.com>
2025-11-26 12:49:35 -0700

    Delete frontend directory

--- frontend/cliente_tienda/src/modulos/personal/api/personalAPI.ts
+++
@@ -1,6 +0,0 @@
-import axios from 'axios';
-
- const personalApi = axios.create({
-    baseURL:'http://localhost:3001/api/personal'
-});
-export default personalApi;
--- frontend/cliente_tienda/src/modulos/personal/interfaces/personal-interface.ts
+++
@@ -1,7 +0,0 @@
-export interface personal{
-    id:number,
-    nombre:string,
-    direccion:string,
-    telefono:string,
-    estatus:number
-}
