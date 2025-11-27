cb5257d7617487383f8dbc23c227a1f46ad8e0d7
Guillermo <146399926+MemoFlashW@users.noreply.github.com>
2025-11-27 13:00:34 -0700

    Update usePersonal.ts

--- frontend/cliente_tienda/src/modulos/personal/controladores/usePersonal.ts
+++ frontend/cliente_tienda/src/modulos/personal/controladores/usePersonal.ts
@@ -4,9 +4,17 @@ import type { Personal, PersonalAgregar } from '../interfaces/personal-interface
 //Modulo de personal
 export const usePersonal = () =>{
     const personal = ref<Personal[]>([]);
+    let mensaje = ref(0);
+
 
     const traePersonal = async () =>{
         const respuesta=await personalApi.get<Personal[]>('/');
+        personal.value = respuesta.data;
+    }
+
+    const traePersonaId = async (id:number) =>{
+        const respuesta = await personalApi.get<Personal[]>('/'+id);
+        console.log(respuesta.data);
         personal.value = respuesta.data;
     }
     const agregarPersonal = async (persona: PersonalAgregar) =>{
@@ -14,12 +22,15 @@ export const usePersonal = () =>{
         const respuesta = await personalApi.post('/',persona);
         if(respuesta.data.affectedRows >= 1){
             console.log('Dato insertado');
+            mensaje.value=1
 
         }
     }
     return {
         personal,
         traePersonal,
-        agregarPersonal
+        agregarPersonal,
+        mensaje,
+        traePersonaId
     }
 }
