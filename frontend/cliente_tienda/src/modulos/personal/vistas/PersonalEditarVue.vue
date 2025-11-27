7471ea5a6beb5dd94be86e77134db4dbdd0399fb
Guillermo <146399926+MemoFlashW@users.noreply.github.com>
2025-11-27 12:58:15 -0700

    Add files via upload

---
+++ frontend/cliente_tienda/src/modulos/personal/vistas/PersonalEditarVue.vue
@@ -0,0 +1,52 @@
+<template>
+    <div class="container mt-5">
+            <div class="card">
+            <div class="card-header">
+                <h4>Editar Personal</h4>
+            </div>
+            <div v-if="mensaje == 1" class="alert alert-success" role="alert">
+                Datos actualizados con exito
+            </div>
+            <div class="card-body">
+                <div class="mb-3">
+                    Nombre
+                    <input type="text" class="form-control" v-model="personal[0].nombre">
+                </div>
+                <div class="mb-3">
+                    Dirección
+                    <input type="text" class="form-control" v-model="personal[0].direccion">
+                </div>
+                <div class="mb-3">
+                    Teléfono
+                    <input type="text" class="form-control" v-model="personal[0].telefono">
+                </div>
+                <div class="mb-3">
+                    Estatus
+                    <input type="text" class="form-control" v-model="personal[0].estatus">
+                </div>
+                <div class="mb-3">
+                    <button class="btn btn-primary" >
+                        Agregar
+                    </button>
+                </div>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script setup lang="ts">
+import { onMounted, ref } from 'vue';
+import type { PersonalAgregar } from '../interfaces/personal-interface';
+import { usePersonal } from '../controladores/usePersonal';
+import { useRoute } from 'vue-router';
+
+const {traePersonaId,personal,mensaje } = usePersonal();
+let idPersona:number=0;
+const route = useRoute();
+
+onMounted (async () =>{
+    idPersona = Number(route.params.id);
+    await traePersonaId(idPersona);
+});
+</script>
+<style scoped></style>
